cf6e08a9325638f6e5310be6a5c9bc28ce70b8bb
SimaoQuintela <simaopedroquintela@hotmail.com>
2023-04-12 19:38:08 +0100

    Added schedule

--- web/src/pages/schedule/index.jsx
+++ web/src/pages/schedule/index.jsx
@@ -1,13 +1,50 @@
-export default function Schedule(){
-    return(
-        <div className="h-screen">
-            <div className="ml-auto mr-auto w-auto text-2xl font-bold">
-                <button type="button" className="px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                    Export
-                </button>
-            </div>
-        </div>
-    );
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from 'moment'
+import Sidebar from "@/components/Sidebar";
 
-    
+const localizer =  momentLocalizer(moment);
+
+const events = [
+  {
+      title: "AUC - TP2",
+      allDay: false,
+      start: moment("2023-04-12T10:00:00").toDate(),
+      end: moment("2023-04-12T12:00:00").toDate()
+  },
+  {
+    title: "PF - TP1",
+    allDay: false,
+    start: moment("2023-04-12T10:00:00").toDate(),
+    end: moment("2023-04-12T12:00:00").toDate()
+}
+]
+
+const MyCalendar = (props) => (
+  <div className="myCustomHeight">
+    <Calendar
+      toolbar={false}
+      localizer={localizer}
+      style={{height:650, margin:"50px"}}
+      defaultDate={new Date()}
+      defaultView={"work_week"}
+      views={["day", "work_week"]}
+      events={events}
+    />
+  </div>
+)
+
+
+export default function BackofficeSchedule(){
+    return(
+        <main>
+          <div className=''>
+            <Sidebar />
+            <div className='relative'>
+              <MyCalendar />
+            </div>
+          </div>
+          
+        </main>
+    );
 }
