0d0e379d6bd040c3ffb9174bc98b60792830d09a
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2023-04-21 01:10:07 +0100

    added display on schedule, que código lindo :cowboy_hat_face: (#18)

---
+++ web/src/pages/api/get/[student_nr].js
@@ -0,0 +1,18 @@
+import alocation from "public/data/schedule.json"
+
+export default function handler(req, res) {
+  const student = req.query
+
+  try{
+    if(Object.keys(alocation).includes(student.student_nr)){
+      let classes = alocation[student.student_nr]
+      res.status(200).json({"classes": classes}) 
+    } else {
+      res.status(404).json({"Error": "Not Found"})
+    }  
+  } catch (err){
+    throw(err)
+  }
+  
+  
+}
--- web/src/pages/schedule/index.jsx
+++ web/src/pages/schedule/index.jsx
@@ -1,31 +1,17 @@
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import Sidebar from "@/components/Sidebar";
+import moment from 'moment'
+import { useEffect, useState } from 'react';
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
-
-import moment from 'moment'
 import 'moment-timezone'
 
 moment.tz.setDefault("Europe/Lisbon")
 
 const localizer =  momentLocalizer(moment);
 
-const events = [
-  {
-      title: "AUC - TP2",
-      allDay: false,
-      start: new Date("2023-04-12T10:00"),
-      end: new Date("2023-04-12T12:30"),
-  },
-  {
-    title: "PF - TP1",
-    allDay: false,
-    start: new Date("2023-04-12T10:00"),
-      end: new Date("2023-04-12T12:30"),
-}
-]
 
-function MyCalendar() {
+function MyCalendar({ events }) {
   const minDate = new Date();
   minDate.setHours(8,0,0);
 
@@ -45,9 +31,16 @@ function MyCalendar() {
         max={maxDate}
         events={events}
         eventPropGetter={(event) => {
+          let color=""
+          if(event.overlap == true){
+            color = "#3F497F"
+          } else {
+            color = "#1775B9"
+          }
+
           const newStyle = {
             border: "solid white",
-            backgroundColor: "#1775B9",
+            backgroundColor: color,
             fontWeight: "500",
             borderRadius: "12px",
             margin: "0"
@@ -60,14 +53,92 @@ function MyCalendar() {
   );
 }
 
+function getDates(slot){
+  let date = new Date();
+  date.toLocaleString('pt', { timeZone: 'Europe/Lisbon' })
+  let year = date.getFullYear()
+  
+  let month = date.getMonth()+1
+  if(month < 10){
+    month = "0"+month
+  }
+
+  let week = {
+    "Monday": 1,
+    "Tuesday": 2,
+    "Wednesday": 3,
+    "Thursday": 4,
+    "Friday": 5
+  }
+  // dia da aula = ( dia do mês + dia da aula - dia da semana atual ) % numero de dias do mês
+  let days_in_month = new Date(year, month,0).getDate()
+  let day = (date.getDate()+ week[slot[0]] - date.getDay()) % days_in_month
+  if(day < 10){
+    day = "0"+day
+  }
+  let start = new Date(year+"-"+month+"-"+day + "T" + slot[1]+":"+ slot[2])
+  let end = new Date(year+"-"+month+"-"+day+"T"+slot[3]+":"+ slot[4])
+
+  return {"start": start, "end": end}
+}
+
+function handleEvents(data) {
+  let events = []
+  Object.values(data.classes).map((lesson) => {
+    lesson.slots.map((slot) => {
+      let dates = getDates(slot) 
+      let event = {
+        "title" : lesson.uc + " - " + lesson.type_class + lesson.shift,
+        "allDay": false,
+        "overlap": slot[5],
+        "start": dates.start,
+        "end": dates.end
+      }
+      events.push(event)
+    });
+  });
+  return events
+}
+
 
 export default function BackofficeSchedule(){
+  let [studentNr, setStudentNr] = useState('');
+  const [evt, setEvt] = useState([]);
+  const axios = require('axios');
+
+  const getSchedule = () => {
+    axios.get('api/get/'+studentNr)
+    .then(response => {
+      let evts = handleEvents(response.data);
+      setEvt(evts);
+      console.log({"200": "Ok"})
+    })
+    .catch((error) =>{
+      setEvt([])
+      if(error.response) console.log(error.response)
+      else if(error.request) console.log(error.request)
+      else console.log({"Error": error.message})
+      console.log(error.config)
+    } )
+  };
+
   return(
       <main className='h-screen bg-white '>
           <Sidebar />
-          <div className='ml-64'>
-            .
-            <MyCalendar />
+          <div className='ml-64 pt-8'>
+            <input type="text"
+                   className='rounded-lg'
+                   value={studentNr}
+                   onChange={(e) => setStudentNr(e.target.value)}
+                   placeholder='Student number'
+            />
+
+            <button type=''
+                    className='bg-[#1775B9] text-white pl-4 pr-4 pt-2 pb-2 ml-2 rounded-lg'
+                    onClick={getSchedule}>
+              Search
+            </button>
+            <MyCalendar events={evt} />
           </div>
       </main>
   );
