6b3f1bf6adf331ec50e948e9ebc3493d9cd5d649
MrNameless10 <miguelfreitas79@hotmail.com>
2023-05-18 12:46:19 +0100

    (Add) new button - UPLOAD

--- web/src/pages/index.jsx
+++ web/src/pages/index.jsx
@@ -38,6 +38,13 @@ export default function Home() {
       }
     });
   };
+
+  const handleGenerateClick2 = () => {
+    console.log("Generate button clicked");
+    fetch('/api/generate-schedule', { method: 'POST' })
+      .then(() => console.log('main.py executed'))
+      .catch(error => console.error(error));
+    }
   
 
   return (
@@ -56,6 +63,12 @@ export default function Home() {
           <UploadButton onFilesSelect={handleUpload(setSelectedFiles3)} />
         </div>
         <button onClick={handleGenerateClick} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            Upload
+          </span>
+        </button>
+
+        <button onClick={handleGenerateClick2} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
             Generate
           </span>
