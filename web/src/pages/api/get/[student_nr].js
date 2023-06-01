a82f539bdfe833e6332a264dac2cc316e175687b
MrNameless10 <miguelfreitas79@hotmail.com>
2023-06-01 15:47:58 +0100

    (Fix) ExportAll

--- web/src/pages/api/get/[student_nr].js
+++ web/src/pages/api/get/[student_nr].js
@@ -1,19 +1,17 @@
-import alocation from "public/data/alocation.json"
+import alocation from "../../../../public/data/alocation.json";
 
 export default function handler(req, res) {
-  const student = req.query
+  const student = req.query;
 
-  try{
-    if(Object.keys(alocation).includes(student.student_nr)){
-      console.log(alocation[student.student_nr])
-      let classes = alocation[student.student_nr]
-      res.status(200).json({"classes": classes}) 
+  try {
+    if (Object.keys(alocation).includes(student.student_nr)) {
+      console.log(alocation[student.student_nr]);
+      let classes = alocation[student.student_nr];
+      res.status(200).json({ classes: classes });
     } else {
-      res.status(404).json({"Error": "Not Found"})
-    }  
-  } catch (err){
-    throw(err)
+      res.status(404).json({ Error: "Not Found" });
+    }
+  } catch (err) {
+    throw err;
   }
-  
-  
 }
