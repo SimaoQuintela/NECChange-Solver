c920c374be45225e7cde926b7fd00c2ab7860659
Carlos Fernandes <carlosdf035@gmail.com>
2025-03-06 13:37:19 +0000

    ..

--- web/src/app/restrictions/page.tsx
+++ web/src/app/restrictions/page.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
 import RowRadioButtonsGroup from "@/components/RowRadioButtonsGroup";
 
 export default function Restrictions() {
-  const [year, setYear] = useState(false);
   return (
     <div className="h-screen bg-slate-200 ">
       <RowRadioButtonsGroup />
